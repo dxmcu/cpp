310bfe62d6fd4eeccc7256d0bb0e543730d2b3a0
hutpond <hutpond@aliyun.com>
2018-12-21 19:06:53 +0800

    add arguments

--- cmakecreator/main.cpp
+++ cmakecreator/main.cpp
@@ -8,11 +8,22 @@ std::string cmake_version();
 int main(int argc, char **argv)
 {
   int ch;
-  while ((ch = getopt(argc, argv, "h")) != -1)
+  std::string project_name = "project_name";
+  std::string program_name = "${PROJECT_NAME}";
+  while ((ch = getopt(argc, argv, "hp:e:")) != -1)
   {
     switch (ch)
     {
     case 'h':
+      std::cout << "-h help" << std::endl;
+      std::cout << "-p project" << "    set project name" << std::endl;
+      std::cout << "-e name" << "    set program name" << std::endl;
+      return 0;
+    case 'p':
+      project_name = optarg;
+      break;
+    case 'e':
+      program_name = optarg;
       break;
     default:
       break;
@@ -23,17 +34,17 @@ int main(int argc, char **argv)
 
   ofs << cmake_version() << std::endl << std::endl;
 
-  ofs << "project(project_name)" << std::endl << std::endl;
+  ofs << "project(" << project_name << ")" << std::endl << std::endl;
 
   ofs << "set (CMAKE_CXX_FLAGS \"${CMAKE_CXX_FLAGS} -std=c++11\")" << std::endl << std::endl;
 
   ofs << "#include_directories(/usr/include)" << std::endl;
   ofs << "#link_directories(/usr/lib)" << std::endl << std::endl;
 
-  ofs << "add_executable(${PROJECT_NAME} main.cpp)" << std::endl;
-  ofs << "#target_sources(${PROJECT_NAME}  PRIVATE test.cpp)" << std::endl;
-  ofs << "#target_link_libraries(${PROJECT_NAME} rt)" << std::endl;
-  ofs << "#target_link_libraries(${PROJECT_NAME} pthread)" << std::endl;
+  ofs << "add_executable(" + program_name + " main.cpp)" << std::endl;
+  ofs << "#target_sources(" + program_name + " PRIVATE test.cpp)" << std::endl;
+  ofs << "#target_link_libraries(" + program_name + " rt)" << std::endl;
+  ofs << "#target_link_libraries(" + program_name + " pthread)" << std::endl;
 
   ofs.close();
 
